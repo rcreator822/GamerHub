0306a153e8ae31920ed9ef29f2be20dbe224f712
Rehan <190790854+rcreator822@users.noreply.github.com>
2025-05-22 19:38:37 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/.gitignore", "/403.html", "/404.html", "/LICENSE", "/README.md", "/TabIcon.png", "/appsredirect.html", "/errors.htaccess", "/games.html", "/gamesredireect.html", "/helios.html", "/indexredirect.html", "/manifest.json", "/proxy.php"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
